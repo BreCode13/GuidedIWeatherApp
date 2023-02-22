e0716e0576b5e3a8a9ff6fa6c2ba8aa1a720c19a
BreCode13 <119532759+BreCode13@users.noreply.github.com>
2023-02-22 16:36:31 -0500

    BootStrap Changes

--- WeatherApp/webserver/src/css.js
+++ WeatherApp/webserver/src/css.js
@@ -1,10 +1,40 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
+//Define paths for Express configur
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+const viewsPath = path.join(__dirname, '../templates')
+const partialsOath = path.join(__dirname, '../templates/partials')
+
+//Setup hbs engine and view location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+
+
+
+
+//Setup static directory to server
 app.use(express.static(publicDirectoryPath))
+//
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Breana Young'
+
+    })
+})
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Breana Young'
+    })
+})
+
 
 app.get('/weather', (req, res) => {
     res.send({
